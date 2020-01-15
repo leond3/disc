6f9a8bbfb3a6f6f1c266538875f2880cbccdad25
leond3 <59891470+leond3@users.noreply.github.com>
2020-01-15 17:57:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,6 +76,6 @@ client.on('message', message => {
 	}
 });
 
-client.user.setActivity('Made by leond3',{ type:'PLAYING'})
+client.user.setActivity('Made by leond3',{type:'PLAYING'})
 
 client.login(process.env.token);
