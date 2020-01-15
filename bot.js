8bd2c9ec3a8104f728ded388fed4c5acc48e0b09
leond3 <59891470+leond3@users.noreply.github.com>
2020-01-15 11:09:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,8 +10,7 @@ client.once('ready', () => {
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	
-	let rolearg = message.guild.roles.find(role => role.name === "${args}");
-	let rolered = message.guild.roles.find(role => role.name === "red");
+	var Role = message.guild.roles.find(r => r.name.toLowerCase() == args[0].toLowerCase());
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
@@ -36,10 +35,7 @@ client.on('message', message => {
 	message.channel.send("**Damage:**\nArmor: Godly\nSword: Spicy\nLegendary/Epic talisman: Godly\nRare talisman: Itchy\nUncommon/Common talisman (*80%< crit chance*): Itchy\nUncommon/Common talisman (*80%>crit chance*): Godly/Zealous\n\n**HP&Defence:**\nArmor: Titanic\nSword: -\nTalisman: Ominous\n\n**Mana:**\nArmor: Wise\nSword: Legendary\nTalisman:Bizarre/Pretty");
 	}
 	if (command === 'color') {
-	message.member.addRole(rolearg);
-	}
-	if (command === 'red') {
-	message.member.addRole(rolered);
+	message.member.addRole(Role);
 	message.delete(1000);
 	}
 });
