2aada849d8ec400ba6d2899878b29c4348ce4025
leond3 <59891470+leond3@users.noreply.github.com>
2020-01-17 13:05:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,21 +125,21 @@ client.on('message', message => {
 		var Amount = getRandomInt(2, 4);
 		var Form = getRandomInt(1,5);
 		
-		if (Form === 1 && Amount == 2) { message.channel.send(getRandomInt(0,6) + ' x ' + getRandomInt(0,6)).then(msg => {msg.delete(15000)}); }
-		if (Form === 1 && Amount == 3) { message.channel.send(getRandomInt(0,6) + ' x ' + getRandomInt(0,6) + ' x ' + getRandomInt(0,6)).then(msg => {msg.delete(15000)}); }
-		if (Form === 1 && Amount == 4) { message.channel.send(getRandomInt(0,6) + ' x ' + getRandomInt(0,6) + ' x ' + getRandomInt(0,6) + ' x ' + getRandomInt(0,6)).then(msg => {msg.delete(15000)}); }
+		if (Form === 1 && Amount == 2) { message.channel.send('wat is: **' + getRandomInt(0,6) + ' x ' + getRandomInt(0,6) + '=**').then(msg => {msg.delete(15000)}); }
+		if (Form === 1 && Amount == 3) { message.channel.send('wat is: **' + getRandomInt(0,6) + ' x ' + getRandomInt(0,6) + ' x ' + getRandomInt(0,6) + '=**').then(msg => {msg.delete(15000)}); }
+		if (Form === 1 && Amount == 4) { message.channel.send('wat is: **' + getRandomInt(0,6) + ' x ' + getRandomInt(0,6) + ' x ' + getRandomInt(0,6) + ' x ' + getRandomInt(0,6) + '=**').then(msg => {msg.delete(15000)}); }
 		
-		if (Form === 2 && Amount == 2) { message.channel.send(getRandomInt(1,21) + ' + ' + getRandomInt(1,21)).then(msg => {msg.delete(15000)}); }
-		if (Form === 2 && Amount == 3) { message.channel.send(getRandomInt(1,21) + ' + ' + getRandomInt(1,21) + ' + ' + getRandomInt(1,21)).then(msg => {msg.delete(15000)}); }
-		if (Form === 2 && Amount == 4) { message.channel.send(getRandomInt(1,21) + ' + ' + getRandomInt(1,21) + ' + ' + getRandomInt(1,21) + ' + ' + getRandomInt(1,21)).then(msg => {msg.delete(15000)}); }
+		if (Form === 2 && Amount == 2) { message.channel.send('wat is: **' + getRandomInt(1,21) + ' + ' + getRandomInt(1,21) + '=**').then(msg => {msg.delete(15000)}); }
+		if (Form === 2 && Amount == 3) { message.channel.send('wat is: **' + getRandomInt(1,21) + ' + ' + getRandomInt(1,21) + ' + ' + getRandomInt(1,21) + '=**').then(msg => {msg.delete(15000)}); }
+		if (Form === 2 && Amount == 4) { message.channel.send('wat is: **' + getRandomInt(1,21) + ' + ' + getRandomInt(1,21) + ' + ' + getRandomInt(1,21) + ' + ' + getRandomInt(1,21) + '=**').then(msg => {msg.delete(15000)}); }
 		
-		if (Form === 3 && Amount == 2) { message.channel.send(getRandomInt(18,50) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10)).then(msg => {msg.delete(15000)}); }
-		if (Form === 3 && Amount == 3) { message.channel.send(getRandomInt(27,75) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10)).then(msg => {msg.delete(15000)}); }
-		if (Form === 3 && Amount == 4) { message.channel.send(getRandomInt(36,100) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10)).then(msg => {msg.delete(15000)}); }
+		if (Form === 3 && Amount == 2) { message.channel.send('wat is: **' + getRandomInt(18,50) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10) + '=**').then(msg => {msg.delete(15000)}); }
+		if (Form === 3 && Amount == 3) { message.channel.send('wat is: **' + getRandomInt(27,75) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10) + '=**').then(msg => {msg.delete(15000)}); }
+		if (Form === 3 && Amount == 4) { message.channel.send('wat is: **' + getRandomInt(36,100) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10) + ' - ' + getRandomInt(1,10) + '=**').then(msg => {msg.delete(15000)}); }
 		
-		if (Form === 4 && Amount == 2) { message.channel.send('Kwadraat van: ' + getRandomInt(1, 16)).then(msg => {msg.delete(15000)}); }
-		if (Form === 4 && Amount == 3) { message.channel.send('Wortel van: 625').then(msg => {msg.delete(15000)}); }
-		if (Form === 4 && Amount == 4) { message.channel.send('Wortel van: 256').then(msg => {msg.delete(15000)}); }
+		if (Form === 4 && Amount == 2) { message.channel.send('Wat is het kwadraat van: **' + getRandomInt(1, 16) + '**?').then(msg => {msg.delete(15000)}); }
+		if (Form === 4 && Amount == 3) { message.channel.send('Wat is de wortel van: **625**?').then(msg => {msg.delete(15000)}); }
+		if (Form === 4 && Amount == 4) { message.channel.send('Wat is de wortel van: **256**?').then(msg => {msg.delete(15000)}); }
 		
 		message.delete();
 	}
