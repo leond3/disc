71bbf7c53e67c26446bd11d40e4b8e756deb4fb6
leond3 <59891470+leond3@users.noreply.github.com>
2020-01-21 12:34:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const { prefix } = require('./config.json');
+
 const fs = require('fs');
-client.data = require('./data.json');
+const data = require('./data.json');
 
 const client = new Discord.Client();
 
