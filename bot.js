47d9c1910dc517975c356bd53d6561fdf32aadea
leond3 <59891470+leond3@users.noreply.github.com>
2020-01-22 20:16:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,11 +130,6 @@ client.on('message', message => {
 		if (coinflip === 2) { message.channel.send(cf[2]).then(msg => {msg.delete(300000)}); }
 		message.delete(300000);
 	}
-	const img_joost = [
-		'https://media.giphy.com/media/CZpro4AZHs436/giphy.gif',
-		'https://media.giphy.com/media/CZpro4AZHs436/giphy2.gif',
-		'https://media.giphy.com/media/CZpro4AZHs436/giphy3.gif',
-		]
 	else if (command === 'image') {
 		if (args[0].toLowerCase() === 'joost') {
 			let chosenFile = files[Math.floor(Math.random() * files.length)]
