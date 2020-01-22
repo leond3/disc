df0ae21435d91a93906e11680b590477f12dcfe2
leond3 <59891470+leond3@users.noreply.github.com>
2020-01-22 14:14:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,9 @@ const { prefix } = require('./config.json');
 
 const client = new Discord.Client();
 
+var fs = require('fs');
+var files = fs.readdirSync('./assets/joost/')
+
 client.once('ready', () => {
 	console.log('MC/ARK responded.');
 });
@@ -127,7 +130,18 @@ client.on('message', message => {
 		if (coinflip === 2) { message.channel.send(cf[2]).then(msg => {msg.delete(300000)}); }
 		message.delete(300000);
 	}
-	
+	const img_joost = [
+		'https://media.giphy.com/media/CZpro4AZHs436/giphy.gif',
+		'https://media.giphy.com/media/CZpro4AZHs436/giphy2.gif',
+		'https://media.giphy.com/media/CZpro4AZHs436/giphy3.gif',
+		]
+	else if (command === 'image') {
+		if (args[0].toLowerCase() === 'joost') {
+			let chosenFile = files[Math.floor(Math.random() * files.length)]
+			message.channel.send(chosenFile).then(msg => {msg.delete(30000)});
+		}
+		message.delete(4000);
+	}
 	
 	
 	
