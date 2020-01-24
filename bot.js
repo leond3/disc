356746ea320f79736b31ce303caf92a48324bbed
leond3 <59891470+leond3@users.noreply.github.com>
2020-01-24 14:22:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -200,6 +200,7 @@ client.on('message', message => {
 		else if (command === 'muteall' && message.member.roles.find(r => r.name === "Bot builder")) {
 			message.channel.overwritePermissions(message.channel.guild.defaultRole, { SEND_MESSAGES: false });
 			message.channel.send("Server chat messages: **Disabled**.").then(msg => {msg.delete(72000000)});
+			message.delete();
 		}
 		else if (command === 'muteall' && !message.member.roles.find(r => r.name === "Bot builder")) {
 			message.channel.send("**You do not have the right permissions to execute this command, try: '!help'.**").then(msg => {msg.delete(4000)});
@@ -208,6 +209,7 @@ client.on('message', message => {
 		else if (command === 'unmuteall' && message.member.roles.find(r => r.name === "Bot builder")) {
 			message.channel.overwritePermissions(message.channel.guild.defaultRole, { SEND_MESSAGES: true });
 			message.channel.send("Server chat messages: **Enabled**.").then(msg => {msg.delete(72000000)});
+			message.delete();
 		}
 		else if (command === 'unmuteall' && !message.member.roles.find(r => r.name === "Bot builder")) {
 			message.channel.send("**You do not have the right permissions to execute this command, try: '!help'.**").then(msg => {msg.delete(4000)});
