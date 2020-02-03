13dcef77c06756370831cf3f2112344ddf75ba5e
leond3 <59891470+leond3@users.noreply.github.com>
2020-02-03 10:50:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('message', message => {
 		}
 		if (command === 'mcnotify' && message.member.roles.find(r => r.name === "Moderator")) {
 			message.delete(4000);
-			message.channel.send(":white_check_mark: Notification detected!\n\n" + message.content.slice(10) + "\n\n*This message will be deleted in 30 minutes.*\n<@&671293618421497868>").then(msg => {msg.delete(4000)});
+			message.channel.send(":white_check_mark: Notification detected!\n\n" + message.content.slice(10) + "\n\n*This message will be deleted in 30 minutes.*\n<@&671293618421497868>").then(msg => {msg.delete(1800000)});
 		}
 		if (command === 'clearchat' && !message.member.roles.find(r => r.name === "Bot builder")) {
 			message.channel.send(":no_entry: **You do not have the right permissions to execute this command, try: '!help'.**").then(msg => {msg.delete(4000)});
