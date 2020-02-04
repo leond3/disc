75b6d4070739194f8be88dbe7dd75d8913e13817
leond3 <59891470+leond3@users.noreply.github.com>
2020-02-04 13:09:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,7 +111,7 @@ client.on('message', message => {
 			const snekfetch = require("snekfetch");
 			snekfetch.get(api).then(r => {
 				let body = r.low;
-				let name = Number(args[0]);
+				let name = args[0];
 				if (!name) { message.channel.send(":no_entry: **Invalid Argument, try: '!help'.**").then(msg => {msg.delete(4000)}); return; }
 				if (isNaN(name)) { message.channel.send(":no_entry: **Invalid Argument, try: '!help'.**").then(msg => {msg.delete(4000)}); return; }
 				let entry = body.find(post => post.id === name)
