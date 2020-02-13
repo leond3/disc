0e89ef63b5bc095f8b22f2cbe94efdb3ff5d3e1b
leond3 <59891470+leond3@users.noreply.github.com>
2020-02-13 09:49:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -328,4 +328,9 @@ client.on('message', message => {
 	}
 });
 
+client.on('messageReactionAdd', (reaction, user) => {
+    if(reaction.emoji.name === "✅") {
+    console.log(reaction.users);
+}
+
 client.login(process.env.token);
