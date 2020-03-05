a6cd01ba757b1933cc7d1503e72c1aceae371201
leond3 <59891470+leond3@users.noreply.github.com>
2020-03-05 12:24:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,7 @@ const client = new Discord.Client();
 client.once('ready', () => {
 	console.log('MineCraft bot responded and is operational!');
 	client.user.setStatus('online');
-	//client.user.setActivity('Made by Leon#1250');
-	client.user.setActivity('is coming...');
+	client.user.setActivity('Made by Leon#1250');
 });
 
 client.on('guildMemberAdd', member => {
